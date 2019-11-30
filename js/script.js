3dab14d9f7509082e0deca877e3028c85632bbec
MateiSR <matei2radu11@gmail.com>
2019-11-30 17:38:14 +0200

    Accordion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function() {
     $('body').removeClass('fade-out');
 });
+
+// scroll down script - ?
+$(function down() {
+    window.scrollY - 300;
+});
+
+$('.collapse').on('hidden.bs.collapse', function() {
+    var defaultDiv = $($(this).data("parent")).data("default");
+    $('.collapse').eq(defaultDiv - 1).collapse('show');
+})
