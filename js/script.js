c0fce178997e08279621f1c0bb33a04e713272e8
Matei Radu <matei2radu11@gmail.com>
2020-01-31 17:23:06 +0200

    site update

--- js/script.js
+++ js/script.js
@@ -10,4 +10,8 @@ $(function down() {
 $('.collapse').on('hidden.bs.collapse', function() {
     var defaultDiv = $($(this).data("parent")).data("default");
     $('.collapse').eq(defaultDiv - 1).collapse('show');
-})
+});
+
+function submit() {
+    alert("test");
+}
